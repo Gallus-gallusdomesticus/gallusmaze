0d828aee1f2d08c558fd401997f4d25c7ded89f6
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-06-05 20:19:33 +1000

    add test file for the maze function

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from windows import Line, Point
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
 
         self.has_left_wall = True
         self.has_bottom_wall = True
@@ -24,24 +24,32 @@ class Cell:
             po1=Point(self.__x1, self.__y1)
             po2=Point(self.__x1, self.__y2)
             li=Line(po1, po2)
+            if self.__win==None:
+                return
             self.__win.draw_line(li, "Black")
         
         if self.has_bottom_wall:
             po1=Point(self.__x1, self.__y1)
             po2=Point(self.__x2, self.__y1)
             li=Line(po1, po2)
+            if self.__win==None:
+                return
             self.__win.draw_line(li, "Black")
         
         if self.has_right_wall:
             po1=Point(self.__x2, self.__y1)
             po2=Point(self.__x2, self.__y2)
             li=Line(po1, po2)
+            if self.__win==None:
+                return
             self.__win.draw_line(li, "Black")
         
         if self.has_top_wall:
             po1=Point(self.__x1, self.__y2)
             po2=Point(self.__x2, self.__y2)
             li=Line(po1, po2)
+            if self.__win==None:
+                return
             self.__win.draw_line(li, "Black")
 
     def get_center_point(self):
@@ -58,6 +66,9 @@ class Cell:
         po2=to_cell.get_center_point()
 
         li=Line(po1, po2)
+
+        if self.__win==None:
+            return
         self.__win.draw_line(li, color)
 
         
